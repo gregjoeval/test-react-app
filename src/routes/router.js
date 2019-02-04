3a0eaf055df1571a69e3162bcfb8c75d6f67f549
Greg Valentine <gvalentine@dealerpolicy.com>
2019-02-04 13:51:20 -0500

    moved component

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,21 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import NotFound from '../screens/notFound';
 import Home from '../screens/home';
 import About from '../screens/about';
+import BrowserRouter from "react-router-dom/es/BrowserRouter";
 
 const AppRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route
-        path={["/home", "/index"]}
-        render={props => <Home {...props} />}
-      />
-      <Route
-        path="/about"
-        render={props => <About {...props} />}
-      />
-      <Route component={NotFound} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route
+          path={["/home", "/index"]}
+          render={props => <Home {...props} />}
+        />
+        <Route
+          path="/about"
+          render={props => <About {...props} />}
+        />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
