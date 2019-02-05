5487627611cb50fee11cb80518393cb99ab58eda
Greg Valentine <gregjoeval@gmail.com>
2019-02-04 22:24:02 -0500

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BrowserRouter from 'react-router-dom/es/BrowserRouter';
 import AppRouter from './routes/router';
 import theme from './theme';
 import {MuiThemeProvider} from '@material-ui/core/styles';
